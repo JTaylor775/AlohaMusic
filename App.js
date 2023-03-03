29e0de39ebeafe97aa2dea4eccab251c757ef525
Jeremiah Taylor <jt708775@southeast.edu>
2023-03-03 16:02:15 -0600

    First commit

--- App.js
+++ App.js
@@ -1,20 +1,154 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { Component } from 'react';
+//import { Audio } from 'expo';
+import { Audio, InterruptionModeAndroid, InterruptionModeIOS } from 'expo-av';
+import { Feather } from '@expo/vector-icons';
+import {
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  View,
+  Image
+} from 'react-native';
+import * as SplashScreen from 'expo-splash-screen';
+import ukulele from './images/ukulele.png';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+SplashScreen.preventAutoHideAsync();
+setTimeout(SplashScreen.hideAsync, 2000);
+
+export default class App extends Component {
+
+  state = {
+    isPlaying: false,
+    playbackInstance: null,
+    volume: 1.0,
+    currentTrackIndex: 0,
+    isBuffering: false,
+  }
+
+	async componentDidMount() {
+    await Audio.setAudioModeAsync({
+      allowsRecordingIOS: false,
+      playThroughEarpieceAndroid: true,
+      interruptionModeIOS: InterruptionModeIOS.DoNotMix,
+      playsInSilentModeIOS: true,
+      shouldDuckAndroid: true,
+      interruptionModeAndroid: InterruptionModeAndroid.DoNotMix,
+    });
+    this.loadAudio();
+  }
+
+  handlePlayPause = async () => {
+    const { isPlaying, playbackInstance } = this.state;
+    isPlaying ? await playbackInstance.pauseAsync() : await playbackInstance.playAsync();
+    this.setState({
+      isPlaying: !isPlaying
+    });
+  }
+
+  onPlaybackStatusUpdate = (status) => {
+    this.setState({
+      isBuffering: status.isBuffering
+    });
+  }
+
+  async loadAudio() {
+    const playbackInstance = new Audio.Sound();
+    const source = require('./music/ukulele.mp3');
+		const status = {
+			shouldPlay: this.state.isPlaying,
+			volume: this.state.volume,
+    };
+    playbackInstance
+      .setOnPlaybackStatusUpdate(
+        this.onPlaybackStatusUpdate
+      );
+    await playbackInstance.loadAsync(source, status, false);
+    this.setState({
+      playbackInstance
+    });
+  }
+
+  renderSongInfo() {
+    const { playbackInstance, currentTrackIndex } = this.state;
+    return playbackInstance ?
+    <View style={styles.trackInfo}>
+      <Text style={[styles.trackInfoText, styles.largeText]}>
+        {playlist[currentTrackIndex].title}
+      </Text>
+      <Text style={[styles.trackInfoText, styles.smallText]}>
+        {playlist[currentTrackIndex].artist}
+      </Text>
+      <Text style={[styles.trackInfoText, styles.smallText]}>
+        {playlist[currentTrackIndex].album}
+      </Text>
     </View>
-  );
+    : null;
+  }
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text style={styles.heading}>Aloha Music</Text>
+        <Image style={styles.image} source={ukulele} />
+        <View style={styles.controls}>
+          <TouchableOpacity
+            style={styles.control}
+            onPress={this.handlePlayPause}
+          >
+            {this.state.isPlaying ?
+              <Feather name="pause" size={32} color="black"/> :
+              <Feather name="play" size={32} color="black"/>
+            }
+          </TouchableOpacity>
+        </View>
+      </View>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#f4e3cf',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  trackInfo: {
+    padding: 40,
+    backgroundColor: '#191A1A',
+  },
+  buffer: {
+    color: '#fff'
+  },
+  trackInfoText: {
+    textAlign: 'center',
+    flexWrap: 'wrap',
+    color: '#fff'
+  },
+  largeText: {
+    fontSize: 22
+  },
+  smallText: {
+    fontSize: 16
+  },
+  control: {
+    margin: 20,
+    top: 10
+  },
+  controls: {
+    flexDirection: 'row'
+  },
+  heading: {
+    width: 300,
+    backgroundColor: "#da9547",
+    padding: 0,
+    fontSize: 30,
+    fontWeight: "bold",
+    textAlign: 'center',
+    bottom: 30
+  },
+  image: {
+    height: 500,
+    width: 300
+  }
 });
